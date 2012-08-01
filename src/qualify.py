49b6c4a83e02f0922af2b036db95ec497f922a61
bytezen <bytezen@rhaz.es>
2012-08-01 21:33:48 +1200

    moved Track and Driver code into Main.  Added methods to qualify module

--- src/qualify.py
+++ src/qualify.py
@@ -6,19 +6,19 @@ Created on Jul 29, 2012
 
 # A QUALIFY RATING TABLE
 qualify_a = [0]*100
-for i in range(9):
+for i in range(10):
     qualify_a[i] = 12
-for i in range(10,19):
+for i in range(10,20):
     qualify_a[i] = 13
-for i in range(20,29):
+for i in range(20,30):
     qualify_a[i] = 14
-for i in range(30,49):
+for i in range(30,50):
     qualify_a[i] = 15
-for i in range(50,65):
+for i in range(50,66):
     qualify_a[i] = 16
-for i in range(66,74):
+for i in range(66,75):
     qualify_a[i] = 17
-for i in range(75,98):
+for i in range(75,99):
     qualify_a[i] = 18
 
 qualify_a[99] = "trouble"
@@ -26,76 +26,76 @@ qualify_a[99] = "trouble"
 
 # B QUALIFY RATING TABLE
 qualify_b = [0]*100
-for i in range(14):
+for i in range(15):
     qualify_b[i] = 12
-for i in range(15,24):
+for i in range(15,25):
     qualify_b[i] = 13
-for i in range(25,34):
+for i in range(25,35):
     qualify_b[i] = 14
-for i in range(35,54):
+for i in range(35,55):
     qualify_b[i] = 15
-for i in range(55,70):
+for i in range(55,71):
     qualify_b[i] = 16
-for i in range(71,79):
+for i in range(71,80):
     qualify_b[i] = 17
-for i in range(80,98):
+for i in range(80,99):
     qualify_b[i] = 18
 
 qualify_b[99] = "trouble"
 
 # C QUALIFY RATING TABLE
 qualify_c = [0]*100
-for i in range(19):
+for i in range(20):
     qualify_c[i] = 12
-for i in range(20,29):
+for i in range(20,30):
     qualify_c[i] = 13
-for i in range(30,39):
+for i in range(30,40):
     qualify_c[i] = 14
-for i in range(40,59):
+for i in range(40,60):
     qualify_c[i] = 15
-for i in range(60,75):
+for i in range(60,76):
     qualify_c[i] = 16
-for i in range(76,84):
+for i in range(76,85):
     qualify_c[i] = 17
-for i in range(85,98):
+for i in range(85,99):
     qualify_c[i] = 18
 
 qualify_c[99] = "trouble"
 
 # D QUALIFY RATING TABLE
 qualify_d = [0]*100
-for i in range(24):
+for i in range(25):
     qualify_d[i] = 12
-for i in range(25,34):
+for i in range(25,35):
     qualify_d[i] = 13
-for i in range(35,44):
+for i in range(35,45):
     qualify_d[i] = 14
-for i in range(45,64):
+for i in range(45,65):
     qualify_d[i] = 15
-for i in range(65,80):
+for i in range(65,81):
     qualify_d[i] = 16
-for i in range(81,89):
+for i in range(81,90):
     qualify_d[i] = 17
-for i in range(90,98):
+for i in range(90,99):
     qualify_d[i] = 18
 
 qualify_d[99] = "trouble"
 
 # E QUALIFY RATING TABLE
 qualify_e = [0]*100
-for i in range(29):
+for i in range(30):
     qualify_e[i] = 12
-for i in range(30,39):
+for i in range(30,40):
     qualify_e[i] = 13
-for i in range(40,49):
+for i in range(40,50):
     qualify_e[i] = 14
-for i in range(50,69):
+for i in range(50,70):
     qualify_e[i] = 15
-for i in range(70,85):
+for i in range(70,86):
     qualify_e[i] = 16
-for i in range(86,94):
+for i in range(86,95):
     qualify_e[i] = 17
-for i in range(95,98):
+for i in range(95,99):
     qualify_e[i] = 18
 
 qualify_e[99] = "trouble"
@@ -154,6 +154,35 @@ speed_speedway = { 48: 187.5,
 #TODO: Implement road course
 
 
+def getSpeedRating(roll,qualifyRating):
+    if qualifyRating == 'A':
+        return qualify_a[roll]
+    elif qualifyRating == 'B':
+        return qualify_b[roll]
+    elif qualifyRating == 'C':
+        return qualify_c[roll]
+    elif qualifyRating == 'D':
+        return qualify_d[roll]
+    elif qualifyRating == 'E':
+        return qualify_e[roll]
+    else:
+        print "Can not get speed for roll=", roll," qualifyRatin = ", qualifyRating
+        return -1
+
+
+def getAverageSpeed(speedRatingTotal, trackType):
+    if trackType == 'speedway':
+        return speed_speedway[speedRatingTotal]
+    elif trackType == 'short':
+        return speed_short[speedRatingTotal]
+    elif trackType == 'super':
+        pass
+    elif trackType == 'road':
+        pass
+    else:
+        print "Can not get average Speed for speedRatingTotal: ", speedRatingTotal, " track type: ",trackType
+
+    return -1
 
 if __name__ == '__main__':
     pass
